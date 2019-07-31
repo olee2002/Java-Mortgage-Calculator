9e0f115944ade60ddd4640c0021dbcd5c2c1c063
babylee2002 <babylee2002@gmail.com>
2019-07-31 16:09:31 -0400

    set up

---
+++ HelloWorld/src/com/oleespackage/Main.java
@@ -0,0 +1,11 @@
+package com.oleespackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 30;
+        int herAge = age;
+        age = 20;
+	System.out.println(age);
+    }
+}
